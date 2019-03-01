4b9298193118fb829b4841704342d718a3628377
runnejus <runnejus@gmail.com>
2019-03-01 16:04:51 -0700

    initial commit

---
+++ src/main/java/com/jrunnells/code/traincodechallenge/TraincodechallengeApplication.java
@@ -0,0 +1,13 @@
+package com.jrunnells.code.traincodechallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraincodechallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraincodechallengeApplication.class, args);
+	}
+
+}
